edba066cb78e42136abd8aacd6d8503605463c33
Bidyut Mondal <61779212+bidyutmondal@users.noreply.github.com>
2021-07-12 12:59:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,40 +18,32 @@ const winningMessageTextElement = document.querySelector('[data-winning-message-
 let circleTurn
 
 //Minimax function
-class Move
-{
-	constructor()
-	{
+class Move{
+	constructor(){
 		let row,col;
 	}
 }
 
 let player = 'o', opponent = 'x';
 
-// This function returns true if there are moves
-// remaining on the grid. It returns false if
-// there are no moves left to play.
-function isMovesLeft(grid)
-{
-	for(let i = 0; i < 3; i++)
-		for(let j = 0; j < 3; j++)
-			if (grid[i][j] == '_')
+//function to check if there are any empty cells returns true if empty cell is present
+function isMovesLeft(grid){
+	for(let i = 0; i < 3; i++){
+		for(let j = 0; j < 3; j++){
+			if (grid[i][j] == '_'){
 				return true;
+			}
+		}
+	}
 				
 	return false;
 }
 
-// This is the evaluation function as discussed
-// in the previous article ( http://goo.gl/sJgv68 )
-function evaluate(b)
-{
+//function to check who is winning
+function evaluate(b){
 	
-	// Checking for Rows for X or O victory.
-	for(let row = 0; row < 3; row++)
-	{
-		if (b[row][0] == b[row][1] &&
-			b[row][1] == b[row][2])
-		{
+	for(let row = 0; row < 3; row++){
+		if (b[row][0] == b[row][1] && b[row][1] == b[row][2]){
 			if (b[row][0] == player)
 				return +10;
 				
@@ -60,12 +52,8 @@ function evaluate(b)
 		}
 	}
 
-	// Checking for Columns for X or O victory.
-	for(let col = 0; col < 3; col++)
-	{
-		if (b[0][col] == b[1][col] &&
-			b[1][col] == b[2][col])
-		{
+	for(let col = 0; col < 3; col++){
+		if (b[0][col] == b[1][col] && b[1][col] == b[2][col]){
 			if (b[0][col] == player)
 				return +10;
 
@@ -74,9 +62,7 @@ function evaluate(b)
 		}
 	}
 
-	// Checking for Diagonals for X or O victory.
-	if (b[0][0] == b[1][1] && b[1][1] == b[2][2])
-	{
+	if (b[0][0] == b[1][1] && b[1][1] == b[2][2]){
 		if (b[0][0] == player)
 			return +10;
 			
@@ -84,9 +70,7 @@ function evaluate(b)
 			return -10;
 	}
 
-	if (b[0][2] == b[1][1] &&
-		b[1][1] == b[2][0])
-	{
+	if (b[0][2] == b[1][1] && b[1][1] == b[2][0]){
 		if (b[0][2] == player)
 			return +10;
 			
@@ -94,58 +78,29 @@ function evaluate(b)
 			return -10;
 	}
 
-	// Else if none of them have
-	// won then return 0
 	return 0;
 }
 
-// This is the minimax function. It
-// considers all the possible ways
-// the game can go and returns the
-// value of the grid
-function minimax(grid, depth, isMax)
-{
-	let score = evaluate(grid);
+// This is the minimax function. It considers all the possible ways the game can go and returns the value of the grid
+function minimax(grid, depth, isMax){
 
-	// If Maximizer has won the game
-	// return his/her evaluated score
+	let score = evaluate(grid);
 	if (score == 10)
 		return score;
 
-	// If Minimizer has won the game
-	// return his/her evaluated score
 	if (score == -10)
 		return score;
 
-	// If there are no more moves and
-	// no winner then it is a tie
 	if (isMovesLeft(grid) == false)
 		return 0;
 
-	// If this maximizer's move
-	if (isMax)
-	{
+	if (isMax){
 		let best = -1000;
-
-		// Traverse all cells
-		for(let i = 0; i < 3; i++)
-		{
-			for(let j = 0; j < 3; j++)
-			{
-				
-				// Check if cell is empty
-				if (grid[i][j]=='_')
-				{
-					
-					// Make the move
+		for(let i = 0; i < 3; i++){
+			for(let j = 0; j < 3; j++){				
+				if (grid[i][j]=='_'){					
 					grid[i][j] = player;
-
-					// Call minimax recursively
-					// and choose the maximum value
-					best = Math.max(best, minimax(grid,
-									depth + 1, !isMax));
-
-					// Undo the move
+					best = Math.max(best, minimax(grid, depth + 1, !isMax));
 					grid[i][j] = '_';
 				}
 			}
@@ -153,30 +108,13 @@ function minimax(grid, depth, isMax)
 		return best;
 	}
 
-	// If this minimizer's move
-	else
-	{
+	else{
 		let best = 1000;
-
-		// Traverse all cells
-		for(let i = 0; i < 3; i++)
-		{
-			for(let j = 0; j < 3; j++)
-			{
-				
-				// Check if cell is empty
-				if (grid[i][j] == '_')
-				{
-					
-					// Make the move
+		for(let i = 0; i < 3; i++){
+			for(let j = 0; j < 3; j++){				
+				if (grid[i][j] == '_'){					
 					grid[i][j] = opponent;
-
-					// Call minimax recursively and
-					// choose the minimum value
-					best = Math.min(best, minimax(grid,
-									depth + 1, !isMax));
-
-					// Undo the move
+					best = Math.min(best, minimax(grid, depth + 1, !isMax));
 					grid[i][j] = '_';
 				}
 			}
@@ -185,43 +123,20 @@ function minimax(grid, depth, isMax)
 	}
 }
 
-// This will return the best possible
-// move for the player
-function findBestMove(grid)
-{
+// This will return the best possible move for the player
+function findBestMove(grid){
 	let bestVal = -1000;
 	let bestMove = new Move();
 	bestMove.row = -1;
 	bestMove.col = -1;
 
-	// Traverse all cells, evaluate
-	// minimax function for all empty
-	// cells. And return the cell
-	// with optimal value.
-	for(let i = 0; i < 3; i++)
-	{
-		for(let j = 0; j < 3; j++)
-		{
-			
-			// Check if cell is empty
-			if (grid[i][j] == '_')
-			{
-				
-				// Make the move
+	for(let i = 0; i < 3; i++){
+		for(let j = 0; j < 3; j++){
+			if (grid[i][j] == '_'){
 				grid[i][j] = player;
-
-				// compute evaluation function
-				// for this move.
 				let moveVal = minimax(grid, 0, false);
-
-				// Undo the move
 				grid[i][j] = '_';
-
-				// If the value of the current move
-				// is more than the best value, then
-				// update best
-				if (moveVal > bestVal)
-				{
+				if (moveVal > bestVal){
 					bestMove.row = i;
 					bestMove.col = j;
 					bestVal = moveVal;
